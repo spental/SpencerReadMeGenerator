1f1221fe412c3586c183bf11010c90c74dabaf99
Mr. Spencer <codekingkyle@gmail.com>
2021-02-02 16:27:30 -0500

    makereadme.js

---
+++ api.js
@@ -0,0 +1,43 @@
+function MakeReadMe (answers) {
+    return ` 
+    # README.md Generator
+   README.md file will be generated from user input, Using Node.js. 
+
+   ${answers.projectTitle}
+
+   ## Description
+   ${answers.description}
+
+   ##Table of Content
+   ${answers.tableOfContents}
+
+   ### Installation
+   ${answers.installation}
+
+   ### Usage
+   ${answers.usage}
+
+   ### Built With
+   * [VScode](https://code.visualstudio.com/) 
+
+   ### Questions
+   ${ansers.questions}
+
+   ### License 
+   ${answers.license}
+
+   ### Contributors
+   ${answers.contributing}
+
+   ###
+
+
+
+
+
+
+
+
+    `
+
+}
